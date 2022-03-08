6447766c8109ae18e48258ae372dc7f4cb436fc5
KirillHtml5 <mr.kiril17@icloud.com>
2022-03-08 17:15:18 +0300

    add (21-22)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to={'/dialogs/1'}>Kirill</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/2'}>Natasha</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/3'}>Nikita</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/4'}>Father</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/5'}>Mother</NavLink>
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+
+            </div>
+
         </div>
     )
 }
